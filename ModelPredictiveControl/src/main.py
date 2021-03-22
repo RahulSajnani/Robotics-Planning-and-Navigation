5f58d42c5f8697cc7479cf592ebeff128b20e880
RahulSajnani <rahulsajnani2010@hotmail.com>
2021-03-22 20:36:03 +0530

    Update plotter

--- ModelPredictiveControl/src/main.py
+++ ModelPredictiveControl/src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cvxpy as cp
+import matplotlib.pyplot as plt
 
 '''
 Authors:
@@ -41,6 +42,54 @@ class MPCOptimizer:
         C2 = (self.x_start[1] - goal[1])**2
 
         return {"A": A, "B": B, "q1": q1, "q2": q2, "C1": C1, "C2": C2}
+
+    def getVelocityConstraints(self, X, Y):
+        '''
+        Get velocity constraints
+        '''
+
+        h = np.ones(self.steps) * self.v_max
+        m = np.zeros(self.steps)
+
+        constraints = [X <= h, 
+                       m <= X, 
+                       Y <= h,
+                       m <= Y]
+        return constraints
+
+    def getObstacleConstraints(self, X, Y, X_star, Y_star):
+        '''
+        Obtain obstacle constraints
+        '''
+
+        constraints = []
+        
+        if len(self.obstacles) > 0:
+            
+            for obs_i in self.obstacles:
+                print(obs_i)
+                prob_matrix = self.getProblemMatrix(obs_i)        
+                
+                A = prob_matrix["A"]
+                B = prob_matrix["B"]
+                q1 = prob_matrix["q1"] 
+                q2 = prob_matrix["q2"] 
+                C1 = prob_matrix["C1"]
+                C2 = prob_matrix["C2"]
+
+                q1_new = q1.copy() + 2 * self.delta_t**2 * np.sum(X_star)
+                q2_new = q2.copy() + 2 * self.delta_t**2 * np.sum(Y_star)
+                
+                # print(X - X_star, X_star.shape)
+                # print(X_star.shape)
+                print(obs_i[2]**2)
+                constraints.append(obs_i[2]**2 <= X_star.T @ A @ X_star + q1.T @ X_star + C1 \
+                                               +  Y_star.T @ B @ Y_star + q2.T @ Y_star + C2 \
+                                               + (2 * A @ X_star + q1_new).T @ (X - X_star.squeeze())   \
+                                               + (2 * B @ Y_star + q2_new).T @ (Y - Y_star.squeeze())   \
+                                                )
+
+        return constraints
 
     def solve(self, verbose = False):
         '''
@@ -93,62 +142,40 @@ class MPCOptimizer:
             # Solve the problem
             problem.solve(verbose = verbose)
 
-            print(X.value)
+            # print(X.value)
             X_star = np.expand_dims(np.array(X.value), axis = -1)
             Y_star = np.expand_dims(np.array(Y.value), axis = -1)
 
-            # print(X_star, Y_star)
+            print(X_star, Y_star)
+            self.plot(X_star, Y_star)
 
         return X_star, Y_star
 
-    def getVelocityConstraints(self, X, Y):
+    
+    
+    def plot(self, X, Y):
         '''
-        Get velocity constraints
-        '''
-
-        h = np.ones(self.steps) * self.v_max
-        m = np.zeros(self.steps)
-
-        constraints = [X <= h, 
-                       m <= X, 
-                       Y <= h,
-                       m <= Y]
-        return constraints
-
-    def getObstacleConstraints(self, X, Y, X_star, Y_star):
-        '''
-        Obtain obstacle constraints
+        Plot the trajectory
         '''
 
-        constraints = []
+        position_X = self.x_start[0] + np.cumsum(X) * self.delta_t
+        position_Y = self.x_start[1] + np.cumsum(Y) * self.delta_t
+
+        position_X = np.hstack((self.x_start[0], position_X))
+        position_Y = np.hstack((self.x_start[1], position_Y))
+
+    
+        plt.plot(position_X[:], position_Y[:])
+        plt.plot(self.x_start[0], self.x_start[1], "ro")
+        plt.plot(self.x_goal[0], self.x_goal[1], "go")
+        plt.show()
+
         
-        if len(self.obstacles) > 0:
-            
-            for obs_i in self.obstacles:
-                print(obs_i)
-                prob_matrix = self.getProblemMatrix(obs_i)        
-                
-                A = prob_matrix["A"]
-                B = prob_matrix["B"]
-                q1 = prob_matrix["q1"] 
-                q2 = prob_matrix["q2"] 
-                C1 = prob_matrix["C1"]
-                C2 = prob_matrix["C2"]
-
-                # print(X - X_star, X_star.shape)
-                # print(X_star.shape)
-                constraints.append(obs_i[2]**2 <= X_star.T @ A @ X_star + q1.T @ X_star + C1 \
-                                    +  Y_star.T @ B @ Y_star + q2.T @ Y_star + C2 \
-                                    + (2 * A @ X_star + 2 * q1).T @ (X - X_star.squeeze())   \
-                                    + (2 * B @ Y_star + 2 * q2).T @ (Y - Y_star.squeeze())   \
-                                    )
-
-        return constraints
 
 if __name__ == "__main__":
 
-    optim = MPCOptimizer(x_start=[0, 0], x_goal=[5, 5], v_max=10, steps=30, obstacles=[[2, 2, 1], [4, 4, 0.5]])
-    optim.solve(verbose = False)
+    optim = MPCOptimizer(x_start=[0, 0], x_goal=[5, 5], v_max=10, steps=30, delta_t = 0.1, obstacles=[[2, 2, 1], [5, 5, 2.0]])
+    optim.solve(verbose = True)
 
 
         
