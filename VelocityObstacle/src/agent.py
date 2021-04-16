ca2cbe7d946bdf87c69880ec873cd67e7440bee4
Rahul Sajnani <rahulsajnani2010@hotmail.com>
2021-04-17 01:56:53 +0530

    Update Robot class

--- VelocityObstacle/src/agent.py
+++ VelocityObstacle/src/agent.py
@@ -4,30 +4,54 @@ import matplotlib.pyplot as plt
 
 class Robot:
 
-    def __init__(self, init_position, radius):
+    def __init__(self, init_position, radius, dt, destination):
         '''
         Initialize the initial position of bot and bot radius
         '''
-       
         self.position = init_position
         self.radius = radius
-    
-    def moveByVelocity_dt(self, velocity, dt):
+        self.velocity = 0
+        self.dt = dt
+        self.destination = destination
+        self.setInitialVelocity()
+
+    def setInitialVelocity(self):
+        '''
+        Set initial velocity towards destination
+        '''
+
+        dir_vector = self.destination - self.position
+
+        norm_dir_vector = 10 * np.linalg.norm(dir_vector)
+        if norm_dir_vector:
+            dir_vector = dir_vector / norm_dir_vector
+        self.velocity = dir_vector
+
+    def setVelocity(self, velocity):
+        '''
+        Set bot velocity
+        '''
+
+        self.velocity = velocity
+
+        
+    def moveByVelocity_dt(self):
         '''
         Move by velocity and given dt
         
         Param:
             velocity - 2 x 1
             dt       - scalar
-        '''
-       
-        self.position = self.position + velocity * dt
+        '''   
+        
+        if np.linalg.norm(self.position - self.destination) < 0.2:
+            self.velocity = 0
+        self.position = self.position + self.velocity * self.dt
 
     def getPosition(self):
         '''
         Get bot position 
         '''
-        
         return self.position
     
     
@@ -35,5 +59,6 @@ class Robot:
 
 if __name__=="__main__":
 
-    bot = Robot(init_position = np.array([2, 1]), radius = 2)
+    destination = np.array([5, 10])
+    bot = Robot(init_position = np.array([2, 1]), radius = 2, dt = 0.1, destination=destination)
     
--- VelocityObstacle/src/main.py
+++ VelocityObstacle/src/main.py
@@ -6,13 +6,33 @@ from agent import *
 class VelocityObstacle:
 
     def __init__(self):
+        
+        
         pass
 
     def getVelocityObstacle(self, robot, obstacle):
-        pass
+        '''
+        Get velocity obstacle of the obstacle
+        '''
+
+        radius = robot.radius + obstacle.radius
+        position = obstacle.position - robot.position
+        velocity = obstacle.velocity - robot.velocity
+    
 
 
 
-if __name__=="__main__" 
+def run():
 
     vo = VelocityObstacle()
+    init_positions_obstacle = [[5, 0], [3, 4]]
+    # obstacles = []
+    destination = np.array([5, 10])
+    bot = Robot(init_position = np.array([2, 1]), radius = 2, dt = 0.1, destination=destination)
+    
+    # obstacle = Robot(init_position = np.array())
+
+if __name__=="__main__":
+
+    run()
+    
